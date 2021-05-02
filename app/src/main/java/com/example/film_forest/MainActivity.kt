45afaea7d63d9e78f66fc09434e99c66f7f0001b
Fionon <fionv2@gmail.com>
2021-05-02 10:23:07 -0400

    linked first button and made new activity files

--- app/src/main/java/com/example/film_forest/MainActivity.kt
+++ app/src/main/java/com/example/film_forest/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.film_forest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.main_screen_button2)
+
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent);
+        }
     }
 }
