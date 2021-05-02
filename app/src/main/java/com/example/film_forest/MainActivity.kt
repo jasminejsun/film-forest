b3a2544be8c3017f2b48e8e774e3983101f8a977
Fionon <fionv2@gmail.com>
2021-05-02 10:40:25 -0400

    linked home and search by page

--- app/src/main/java/com/example/film_forest/MainActivity.kt
+++ app/src/main/java/com/example/film_forest/MainActivity.kt
@@ -8,7 +8,7 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.menu_screen)
 
         val button: Button = findViewById(R.id.main_screen_button2)
 
--- app/src/main/java/com/example/film_forest/MainActivity2.kt
+++ app/src/main/java/com/example/film_forest/MainActivity2.kt
@@ -1,11 +1,20 @@
 package com.example.film_forest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_by)
+
+        val backButton:ImageView = findViewById(R.id.back_arrow)
+
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent);
+        }
     }
 }
